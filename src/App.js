c14397a0e59dcc4fe94725dbfcb15475ad3ddeb1
Hoora <hoora@Hooras-MacBook-Pro.local>
2022-11-10 00:00:07 -0700

    add search field to state

--- src/App.js
+++ src/App.js
@@ -7,23 +7,26 @@ import { friends } from './friends';
 class App extends Component {
     constructor() {
         super();
-        this.state = {friends: friends}
+        this.state = {
+            friends: friends,
+            searchField: ''
+        }
     }
 
     onSearchChange = (event) => {
-        const searchedFriend = event.target.value;
-        const filteredFriends = friends.filter(friend => {
-            return (friend.name.toLowerCase().includes(searchedFriend.toLowerCase()))
-        });
-        this.setState({friends: filteredFriends});
+        this.setState({searchField: event.target.value});
+
     }
 
     render() {
+        const filteredFriends = this.state.friends.filter(friend => {
+            return (friend.name.toLowerCase().includes(this.state.searchField.toLowerCase()))
+        });
         return (
             <div className = "tc">
                 <h1 className='f2'>My Friends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList friends={this.state.friends}/>
+                <CardList friends={filteredFriends}/>
             </div>
         );
     };
