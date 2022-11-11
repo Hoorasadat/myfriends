b757c7e814a2a641c6e3f28148c4f83c1f0d2278
Hoora <hoora@Hooras-MacBook-Pro.local>
2022-11-10 22:27:20 -0700

    adding loading

--- src/App.js
+++ src/App.js
@@ -30,15 +30,19 @@ class App extends Component {
         const filteredFriends = this.state.friends.filter(friend => {
             return (friend.name.toLowerCase().includes(this.state.searchField.toLowerCase()))
         });
-        return (
-            <div className = "tc">
-                <h1 className='f2'>My Friends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <Scroll>
-                    <CardList friends={filteredFriends}/>
-                </Scroll>
-            </div>
-        );
+        if (this.state.friends.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className = "tc">
+                    <h1 className='f2'>My Friends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <CardList friends={filteredFriends}/>
+                    </Scroll>
+                </div>
+            );
+        }
     };
 }
 
