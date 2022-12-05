7db10d805f02b2fd38374b93e4e1d242d5d0dead
Hoora <hoora@Hooras-MacBook-Pro.local>
2022-12-05 15:14:18 -0700

    using hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, {Component} from 'react';
+import React, {
+    // useEffect,
+    useState} from 'react';
 import './App.css';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
@@ -6,45 +8,36 @@ import { friends } from './friends';
 import Scroll from './Scroll';
 import ErrorBoundry from './ErrorBoundry';
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            friends: friends,
-            // friends: [], // robofriends
-            searchField: ''
-        }
-    }
+function App() {
+    const myFriends = friends;
+    // const [myFriends, setMyFriends] = useState([]);
+    const [searchField, setSearchField] = useState('');
 
-    // componentDidMount() { // robofriends
+    // useEffect(() => { // robofriends
     //     fetch('https://jsonplaceholder.typicode.com/users')
     //     .then(response => response.json())
-    //     .then(users => this.setState({friends: users}));
-    // }
+    //     .then(users => setMyFriends(users));
+    // },[])
 
-    onSearchChange = (event) => {
-        this.setState({searchField: event.target.value});
-
+    const onSearchChange = (event) => {
+        setSearchField(event.target.value);
     }
 
-    render() {
-        const { friends, searchField} = this.state;
-        const filteredFriends = friends.filter(friend => {
-            return (friend.name.toLowerCase().includes(searchField.toLowerCase()))
-        });
-        return !friends.length ?
-        <h1>Loading</h1> :
-        <div className = "tc">
-            <h1 className='f2'>My Friends</h1>
-            <SearchBox searchChange={this.onSearchChange}/>
-            <Scroll>
-                <ErrorBoundry>
-                    <CardList friends={filteredFriends}/>
-                </ErrorBoundry>
-            </Scroll>
-        </div>
-        ;
-    };
+    const filteredFriends = myFriends.filter(friend => {
+        return (friend.name.toLowerCase().includes(searchField.toLowerCase()))
+    });
+    return !myFriends.length ?
+    <h1>Loading</h1> :
+    <div className = "tc">
+        <h1 className='f2'>My Friends</h1>
+        <SearchBox searchChange={onSearchChange}/>
+        <Scroll>
+            <ErrorBoundry>
+                <CardList friends={filteredFriends}/>
+            </ErrorBoundry>
+        </Scroll>
+    </div>
+    ;
 }
 
 export default App;
